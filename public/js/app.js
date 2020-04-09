ae3c9fc753ca492b988e8cbc4560c7858586079b
bangjuyoung <7979456+bangjuyoung@users.noreply.github.com>
2020-04-09 23:45:13 +0900

    EPISODE 2 - Building the Layout
    refer to https://laracasts.com/series/laravel-vue-and-spas/episodes/2

--- public/js/app.js
+++ public/js/app.js
@@ -1964,6 +1964,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "Home"
 });
@@ -37411,7 +37413,11 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_vm._v("Home")])
+  return _c(
+    "h1",
+    { staticClass: "font-normal text-3xl text-gray-700 leading-none" },
+    [_vm._v("\n    Full Logo\n")]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,67 +14,48 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:400,700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="font-sans">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <div class="container mx-auto">
+            <header class="mb-8 py-6">
+                <h1>
+                    <img src="{{ asset('images/logo.svg') }}" alt="Laracasts">
+                </h1>
+            </header>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <main class="flex">
+                <aside class="w-1/5">
+                    <section class="mb-8">
+                        <h5 class="mb-3 uppercase font-bold">The Brand</h5>
+                        <ul>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/">Logo</router-link></li>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/about">Logo Symbol</router-link></li>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/about">Colors</router-link></li>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/about">Typography</router-link></li>
+                        </ul>
+                    </section>
 
-                    </ul>
+                    <section>
+                        <h5 class="mb-3 uppercase font-bold">Doodles</h5>
+                        <ul>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/">Mascot</router-link></li>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/about">Illustrations</router-link></li>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/about">Loaders and Animations</router-link></li>
+                            <li class="leading-10 text-sm"><router-link class="text-black" to="/about">Wallpapers</router-link></li>
+                        </ul>
+                    </section>
+                </aside>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                <div class="primary flex-1">
+                    <router-view></router-view>
                 </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+            </main>
+        </div>
     </div>
 </body>
 </html>
